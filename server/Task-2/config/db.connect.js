e7fd2fd57367a39e97c5e0d9f63870888bcb65c3
keyready <vip.korchak@list.ru>
2023-12-16 12:32:07 +0300

    добавил env во второй таск

--- server/Task-2/config/db.connect.js
+++ server/Task-2/config/db.connect.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config({ path: "./Task-2/.env" });
 
-module.exports = new Sequelize("todo-list", "postgres", "UserSQL", {
-  dialect: "postgres",
-  host: "localhost",
-  port: 5432,
-});
+module.exports = new Sequelize(
+  process.env.PG_TABLE,
+  process.env.PG_USER,
+  process.env.PG_PASS,
+  {
+    dialect: "postgres",
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+  }
+);
