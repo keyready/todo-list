57b74b04a5142fc97ae9e3269f2ca10334012f56
keyready <vip.korchak@list.ru>
2023-12-16 11:15:44 +0300

    добавил ветку с решениями

---
+++ server/Task-1/index.js
@@ -0,0 +1,67 @@
+/**
+ * Вот возможное решение задачи, которое я описал в тз.
+ * Тут, в принципе, все хорошо, ровненько. Можешь изучать
+ *
+ * В идеале, конечно, подключать базу данных и хранить тудушки там, но это
+ * следующее задание)
+ */
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const port = 5000;
+
+const app = express();
+
+const todosList = [
+  { id: 1, title: "Закрыть сессию", status: "active" },
+  { id: 2, title: "Сделать домашку", status: "active" },
+  { id: 3, title: "Покормить кота", status: "active" },
+];
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+app.get("/get_todos", (req, res) => {
+  return res.status(200).json(todosList);
+});
+
+app.post("/create_todo", (req, res) => {
+  const title = req.body.title;
+
+  todosList.push({
+    id: todosList.length + 1,
+    title,
+    status: "active",
+  });
+
+  return res.status(201).json(todosList);
+});
+
+app.post("/complete_todo", (req, res) => {
+  const todoId = req.body.todoId;
+
+  todosList.map((todo) => {
+    if (todo.id === todoId) {
+      todo.status = "completed";
+    }
+  });
+
+  return res.status(200).json(todosList);
+});
+
+app.post("/delete_todo", (req, res) => {
+  const todoId = req.body.todoId;
+
+  const foundIndex = todosList.findIndex((todo) => todo.id === todoId);
+  if (foundIndex !== -1) {
+    todosList.splice(foundIndex, 1);
+  }
+
+  return res.status(200).json(todosList);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
